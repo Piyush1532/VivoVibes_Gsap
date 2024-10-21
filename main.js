645fc3dbdb5c913d21cfe45b4059599b0272a52e
Piyush1532 <piyushadav503@gmail.com>
2024-10-21 16:12:46 +0530

    added some gsap

--- main.js
+++ main.js
@@ -10,3 +10,24 @@ cross.addEventListener("click",()=>{
     navbar.classList.remove("active")
 })
 
+
+
+// gsap start here
+
+const links = document.querySelectorAll('.box-gap a, #navbar a');
+
+// const tl = gsap.timeline({
+//   defaults: { duration: 0.5, ease: "power2.out" }
+// });
+const timeLine=gsap.timeline()
+
+timeLine.fromTo('.imgLogo', 
+  { y: -200, opacity: 0 }, 
+  { y: 0, opacity: 1 }  
+);
+
+timeLine.fromTo(links, 
+  { y: -100, opacity: 0 }, 
+  { y: 0, opacity: 1, stagger: 0.1 }, 
+  "-=0.3"
+);
